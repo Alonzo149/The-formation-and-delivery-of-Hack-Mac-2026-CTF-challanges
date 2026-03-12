cafb7b6d0a6c26100ce3156a12aa80ccb6c1d3b3
secret3242 <macbibi123@gmail.com>
2026-03-12 20:51:33 +1100

    add Docker

--- Challenge1/server.js
+++ Challenge1/server.js
@@ -1,16 +1,17 @@
 const h = require('express');
+const path = require('path');
 const app= h();
 
-const PORT = 3000;
+const PORT = 8000;
 
 app.get('/',(req,res) => {
-    res.sendFiles(__dirname + '/views/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.get('admin',(req,res)=> {
-    res.sendFiles(__dirname + '/views/admin.html');
+app.get('/admin',(req,res)=> {
+    res.sendFile(path.join(__dirname, 'admin', 'admin.html'));
 })
 
-app.listen(8000, () => {
-    console.log('Server running at https://localhost:8000');
+app.listen(8000,'0.0.0.0', () => {
+    console.log('Server running at http://localhost:8000');
 })
