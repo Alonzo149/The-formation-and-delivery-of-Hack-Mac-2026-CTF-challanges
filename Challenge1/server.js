870e1327ab5033b9bf3866c398072db56414e7b9
Samuelchung1 <samuel.chung1@students.mq.edu.au>
2026-03-13 01:51:57 +0000

    Debugging

--- Challenge1/server.js
+++ Challenge1/server.js
@@ -1,17 +1,15 @@
-const h = require('express');
-const path = require('path');
+const h = require("express");
+const path = require("path");
 const app= h();
 
-const PORT = 8000;
-
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 
-app.get('/admin',(req,res)=> {
-    res.sendFile(path.join(__dirname, 'admin', 'admin.html'));
+app.get("/admin.html",(req,res)=> {
+    res.sendFile(path.join(__dirname, "admin", "admin.html"));
 })
 
-app.listen(8000,'0.0.0.0', () => {
-    console.log('Server running at http://localhost:8000');
+app.listen(8000,() => {
+    console.log("Server running at port http://localhost:8000");
 })
