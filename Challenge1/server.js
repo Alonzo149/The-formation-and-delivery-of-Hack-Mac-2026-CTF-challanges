27a7d8ab40d15f746292b5e9274e41f1c5771172
Samuelchung1 <samuel.chung1@students.mq.edu.au>
2026-03-13 02:32:00 +0000

    Fixing some coding

--- Challenge1/server.js
+++ Challenge1/server.js
@@ -1,9 +1,9 @@
-const h = require("express");
+const express = require("express");
 const path = require("path");
-const app= h();
+const app= express();
 
 
-app.use(h.static(__dirname));
+app.use(express.static(__dirname));
 
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname, "index.html"));
