7bceaab36b94a7efb5b075feec8f12d429d5b797
Samuelchung1 <samuel.chung1@students.mq.edu.au>
2026-03-14 17:28:37 +1100

    moving folders

--- Challenge1/server.js
+++ Challenge1/server.js
@@ -7,11 +7,11 @@ app.use(express.static(__dirname));
 
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname, "index.html"));
-})
+});
 
 app.get("/admin.html",(req,res)=> {
     res.sendFile(path.join(__dirname, "admin", "admin.html"));
-})
+});
 
 app.listen(8000,"0.0.0.0",() => {
     console.log("Server running at port http://localhost:8000");
