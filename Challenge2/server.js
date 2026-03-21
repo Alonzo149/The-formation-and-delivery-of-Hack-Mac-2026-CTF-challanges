589187f1400fe1f5cb7edab0b6a9e24f8d1d1954
Samuelchung1 <samuel.chung1@students.mq.edu.au>
2026-03-21 04:43:50 +0000

    Making the challenge2 harder

--- Challenge2/server.js
+++ Challenge2/server.js
@@ -13,6 +13,15 @@ db.serialize(() => {
     db.run("CREATE TABLE users (username TEXT, password TEXT)");
     db.run("INSERT INTO users VALUES ('admin', 'supersecret')");
 });
+function Blocked(filter) {
+    const blocked = ["or","OR"];
+    for (let i = 0; i < blocked.length; i++){
+        if (filter.includes(blocked[i])){
+            return true;
+        }
+    }    return false;
+}
+
 
 app.get("/challenge2", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "login.html"));
@@ -20,11 +29,19 @@ app.get("/challenge2", (req, res) => {
 
 app.post("/challenge2/login", (req, res) => {
     const { username, password } = req.body;
-
+    
+    if (Blocked(username) || Blocked(password)){ 
+        res.send("Input contains blocked keywords");
+        return;
+    }
     //vulnerable query
     const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
 
     db.get(query, (err, row) => {
+        if (err) {
+            res.send("Database error");
+            return;
+        }
         if (row) {
             res.sendFile(path.join(__dirname, "views", "flag2.html"));
         } else {
@@ -34,5 +51,5 @@ app.post("/challenge2/login", (req, res) => {
 });
 
 app.listen(8000, "0.0.0.0", () => {
-    console.log("Running on port http://localhost:8001/challenge2");
+    console.log("Running on port http://localhost:8000/challenge2");
 });
