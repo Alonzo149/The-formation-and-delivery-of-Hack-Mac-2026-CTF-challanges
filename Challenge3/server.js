494865e84d0dfd4bfb53260755751812ae954cf2
Samuelchung1 <samuel.chung1@students.mq.edu.au>
2026-04-03 11:11:06 +0000

    Adding .pcap. and other stuff

---
+++ Challenge3/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app= express();
+
+
+app.use(express.static(path.join(__dirname,"views")));
+
+app.get("/challenge3",(req,res) => {
+    res.sendFile(path.join(__dirname,"views","index.html"));
+})
+
+app.listen(5002,() => {
+    console.log("Server running at port http://localhost:5002/challenge3");
+})
